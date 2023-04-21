9ad7d96d59ed705ff0d8a81a3ef6842fc355d28b
Joanna <joufengchen15.gmail.com>
2023-04-21 18:13:13 +0800

    整理 configUI

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     let redView = UIView()
@@ -22,22 +23,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configUI()
-       
-        redView.backgroundColor = .red
-        view.addSubview(redView)
-        redView.snp.makeConstraints { make in
-            make.leading.equalToSuperview()
-            make.height.equalToSuperview()
-            make.width.equalToSuperview().multipliedBy(0.5)
-        }
-        
-        blueView.backgroundColor = .blue
-        view.addSubview(blueView)
-        blueView.snp.makeConstraints { make in
-            make.trailing.equalToSuperview()
-            make.height.equalToSuperview()
-            make.width.equalToSuperview().multipliedBy(0.5)
-        }
         
 //        changeSides.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0.5)
 //        changeSides.setTitle("", for: .normal)
@@ -50,61 +35,11 @@ class ViewController: UIViewController {
 //            make.size.equalTo(50)
 //        }
         
-        restart.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0.8)
-        restart.setTitle("RESTART", for: .normal)
-        restart.titleLabel?.font = .systemFont(ofSize: 32)
-        restart.layer.cornerRadius = 110
-        restart.layer.borderWidth = 2
-        restart.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
-        
-        redScoreButton.setTitle("\(redScore)", for: .normal)
-        redScoreButton.titleLabel?.font = .systemFont(ofSize: 150)
-        redView.addSubview(redScoreButton)
-        
-        redScoreButton.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.size.equalToSuperview()
-        }
-        
-        blueScoreButton.setTitle("\(blueScore)", for: .normal)
-        blueScoreButton.titleLabel?.font = .systemFont(ofSize: 150)
-        blueView.addSubview(blueScoreButton)
-        
-        blueScoreButton.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.size.equalToSuperview()
-        }
-        
-        redUndoButton.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0.5)
-        redUndoButton.setTitle("undo", for: .normal)
-        redUndoButton.layer.cornerRadius = 12
-        redView.addSubview(redUndoButton)
-        
-        redUndoButton.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().inset(56)
-            make.height.equalTo(35)
-            make.width.equalTo(100)
-        }
-        
-        blueUndoButton.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0.5)
-        blueUndoButton.setTitle("undo", for: .normal)
-        blueUndoButton.layer.cornerRadius = 12
-        blueView.addSubview(blueUndoButton)
-        
-        blueUndoButton.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().inset(56)
-            make.height.equalTo(35)
-            make.width.equalTo(100)
-        }
-                
         redScoreButton.addTarget(self, action: #selector(redScorePressed(_:)), for: .touchUpInside)
         blueScoreButton.addTarget(self, action: #selector(blueScorePressed(_:)), for: .touchUpInside)
         redUndoButton.addTarget(self, action: #selector(redUndoPressed(_:)), for: .touchUpInside)
         blueUndoButton.addTarget(self, action: #selector(blueUndoPressed(_:)), for: .touchUpInside)
         restart.addTarget(self, action: #selector(restartPressed(_:)), for: .touchUpInside)
-        
     }
     
     func showRestart() {
@@ -168,6 +103,12 @@ class ViewController: UIViewController {
 private extension ViewController {
     func configUI() {
         configRedView()
+        configBlueView()
+        configRestart()
+        configRedScoreButton()
+        configBlueScoreButton()
+        configRedUndoButton()
+        configBlueUndoButton()
     }
     
     func configRedView() {
@@ -177,6 +118,75 @@ private extension ViewController {
             make.leading.equalToSuperview()
             make.height.equalToSuperview()
             make.width.equalToSuperview().multipliedBy(0.5)
+        }
+    }
+    
+    func configBlueView() {
+        blueView.backgroundColor = .blue
+        view.addSubview(blueView)
+        blueView.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+            make.height.equalToSuperview()
+            make.width.equalToSuperview().multipliedBy(0.5)
+        }
+    }
+    
+    func configRestart() {
+        restart.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0.8)
+        restart.setTitle("RESTART", for: .normal)
+        restart.titleLabel?.font = .systemFont(ofSize: 32)
+        restart.layer.cornerRadius = 110
+        restart.layer.borderWidth = 2
+        restart.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+    }
+    
+    func configRedScoreButton() {
+        redScoreButton.setTitle("\(redScore)", for: .normal)
+        redScoreButton.titleLabel?.font = .systemFont(ofSize: 150)
+        redView.addSubview(redScoreButton)
+        
+        redScoreButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalToSuperview()
+        }
+    }
+    
+    func configBlueScoreButton() {
+        blueScoreButton.setTitle("\(blueScore)", for: .normal)
+        blueScoreButton.titleLabel?.font = .systemFont(ofSize: 150)
+        blueView.addSubview(blueScoreButton)
+        
+        blueScoreButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalToSuperview()
+        }
+    }
+    
+    func configRedUndoButton() {
+        redUndoButton.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0.5)
+        redUndoButton.setTitle("undo", for: .normal)
+        redUndoButton.layer.cornerRadius = 12
+        redView.addSubview(redUndoButton)
+        
+        redUndoButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().inset(56)
+            make.height.equalTo(35)
+            make.width.equalTo(100)
+        }
+    }
+    
+    func configBlueUndoButton() {
+        blueUndoButton.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0.5)
+        blueUndoButton.setTitle("undo", for: .normal)
+        blueUndoButton.layer.cornerRadius = 12
+        blueView.addSubview(blueUndoButton)
+        
+        blueUndoButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().inset(56)
+            make.height.equalTo(35)
+            make.width.equalTo(100)
         }
     }
 }
